365edbfd5773c44de830c160e5811d4baaf60c69
Gabriel Blake <61571088+gabrielblake28@users.noreply.github.com>
2026-05-01 09:51:21 -0700

    remove trace field from Options and base Pass / Fail Decisions

--- src/engine/engine.types.ts
+++ src/engine/engine.types.ts
@@ -2,47 +2,26 @@ import { Trace } from "../condition/condition";
 
 export enum Kind {
   PASS = "pass",
-  FAIL = "fail"
+  FAIL = "fail",
 }
 
-// Pass
-interface PassWithTrace {
-  result: Kind.PASS,
-  passed: string[],
-  failed?: string[],
-  trace: Trace[],
+interface Pass {
+  result: Kind.PASS;
+  passed: string[];
 }
 
-interface PassWithoutTrace {
-  result: Kind.PASS,
-  passed: string[],
+interface Fail {
+  result: Kind.FAIL;
+  passed: string[];
+  failed: string[];
 }
 
-type Pass = PassWithTrace | PassWithoutTrace;
-
-// Fail
-interface FailWithTrace {
-  result: Kind.FAIL,
-  passed: string[],
-  failed: string[],
-  trace: Trace[],
-}
-
-interface FailWithoutTrace {
-  result: Kind.FAIL,
-  passed: string[],
-  failed: string[],
-}
-
-type Fail = FailWithTrace | FailWithoutTrace
-
-export type Decision = Pass | Fail
+export type Decision = Pass | Fail;
+export type DecisionWithTrace = Decision & { trace: Trace[] };
 
 const strategy = ["all", "allPass", "anyPass"] as const;
 export type Strategy = typeof strategy[number];
 
 export type Options = {
-  trace?: boolean
-  strategy?: Strategy
-}
-
+  strategy?: Strategy;
+};
